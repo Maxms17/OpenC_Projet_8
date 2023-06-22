fb8b54a3eaa0794e8375a1ce98b7ffdce7f68494
Maxms17 <61410393+Maxms17@users.noreply.github.com>
2023-06-22 14:01:21 +0200

    Ajout bannière

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,5 +1,14 @@
+import logo from '../assets/LOGO.jpg'
+import '../styles/Banner.css'
+
 function Banner() {
-    return <h1>Le projet 8</h1>
+    const title = 'Le projet 8 React'
+    return (
+        <div className='titre-banner'>
+            <img src={logo} alt='Kasa' className='titre-logo' />
+            <h1 className='titre-title'>{title}</h1>
+        </div>
+    )
 }
 
 export default Banner
