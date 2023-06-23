0d95ce1adcd265de959da4850688db5718664aa9
Maxms17 <61410393+Maxms17@users.noreply.github.com>
2023-06-23 17:09:04 +0200

    Ajout page et header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,20 @@
 import { Link } from 'react-router-dom'
- 
+import logo from '../../assets/LOGO.jpg'
+import '../../styles/Banner.css'
+
 function Header() {
     return (
-        <nav>
-            <Link to="/pages/Home">Accueil</Link>
-            <Link to="/pages/Logement">Logement</Link>
-            <Link to="/pages/Propos">Propos</Link>
-            <Link to="/pages/Erreur">Erreur</Link>
-        </nav>
+        <div className='my-banner'>
+            <img src={logo} alt='Kasa' className='my-logo' />
+            <nav>
+                <Link to="/pages/Home">Accueil</Link>
+                <Link to="/pages/Logement">Logement</Link>
+                <Link to="/pages/Propos">Propos</Link>
+                <Link to="/pages/Erreur">Erreur</Link>
+            </nav>
+        </div>
     )
 }
 
 export default Header
+
