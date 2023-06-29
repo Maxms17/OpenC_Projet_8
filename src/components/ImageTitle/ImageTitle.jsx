20722327e5c383b7a3d41598571df512916b2631
Maxms17 <61410393+Maxms17@users.noreply.github.com>
2023-06-29 15:15:27 +0200

    Modification body avec imagetitle

--- src/components/ImageTitle/ImageTitle.jsx
+++ src/components/ImageTitle/ImageTitle.jsx
@@ -4,8 +4,8 @@ import Img_Accueil from '../../assets/Img_Accueil.jpg'
 function ImageTitle() {
     return (
         <div className='my-ImageTitle'>
-            <img src={Img_Accueil} alt='Kasa' className='my-Img_Accueil'> <p> Chez vous, partout et ailleurs </p> </img>
-           
+            <img src={Img_Accueil} alt='Kasa' className='my-Img_Accueil' />
+            <p>Chez vous, partout et ailleurs</p>
         </div>
     )
 }
