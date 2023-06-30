6cdd5d9447b3cc95a19496e871ebae34312a1ad3
Maxms17 <61410393+Maxms17@users.noreply.github.com>
2023-06-30 11:58:38 +0200

    Modification action drop

--- src/components/Panneau/Panneau.jsx
+++ src/components/Panneau/Panneau.jsx
@@ -11,18 +11,65 @@ function Panneau() {
     
     return (
         <div className="my-dropdown">
-            <button onClick={handleOpen}>Dropdown</button>
-            {open ? (
-                <ul className="menu">
-                    <li className="menu-item">
-                        <button>Menu 1</button>
-                    </li>
-                    <li className="menu-item">
-                        <button>Menu 2</button>
-                    </li>
-                </ul>
-            ) : null}
-            {open ? <div>Is Open</div> : <div>Is Closed</div>}
+            <button onClick={handleOpen}>Fiabilité</button>
+
+                {open ? (
+                    <ul className="menu">
+                        <li className="menu-item">
+                            <p>
+                            Les annonces postées sur Kasa garantissent une fiabilité totale. 
+                            Les photos sont conformes aux logements, et toutes les informations 
+                            sont régulièrement vérifiées par nos équipes.
+                            </p>
+                        </li>
+                    </ul>
+                ) : null}
+                {open ? <div className="menu-open"> </div> : <div className="menu-close"> </div>}
+
+            <button onClick={handleOpen}>Respect</button>
+                {open ? (
+                    <ul className="menu">
+                        <li className="menu-item">
+                            <p>
+                            La bienveillance fait partie des valeurs fondatrices de Kasa.
+                            Tout comportement discriminatoire ou de perturbation du voisinage
+                            entraînera une exclusion de notre plateforme.
+                            </p>
+                        </li>
+                    </ul>
+                ) : null}
+                {open ? <div className="menu-open"> </div> : <div className="menu-close"> </div>}
+
+            <button onClick={handleOpen}>Service</button>
+                {open ? (
+                    <ul className="menu">
+                        <li className="menu-item">
+                            <p>
+                            La bienveillance fait partie des valeurs fondatrices de Kasa.
+                            Tout comportement discriminatoire ou de perturbation du voisinage
+                            entraînera une exclusion de notre plateforme.
+                            </p>
+                        </li>
+                    </ul>
+                ) : null}
+                {open ? <div className="menu-open"> </div> : <div className="menu-close"> </div>}
+
+            <button onClick={handleOpen}>Sécurité</button>
+                {open ? (
+                    <ul className="menu">
+                        <li className="menu-item">
+                            <p>
+                            La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs,
+                            chaque logement correspond aux critères de sécurité établis par nos services. En laissant une
+                            note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les 
+                            standards sont bien respectés. Nous organisons également des ateliers sur la 
+                            sécurité domestique pour nos hôtes.
+                            </p>
+                        </li>
+                    </ul>
+                ) : null}
+                {open ? <div className="menu-open"> </div> : <div className="menu-close"> </div>}
+
         </div>
     );
 }
