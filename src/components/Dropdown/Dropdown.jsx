4af612f5f57ab0de4097bf9d0b2bff78d33f8ed8
Maxms17 <61410393+Maxms17@users.noreply.github.com>
2023-07-03 16:13:25 +0200

    Ajout dropdown

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,30 +1,31 @@
-const Users = () => {
+import React, { useState } from 'react';
+import Dropdown from '../Dropdown/Dropdown';
 
-    const data = [
-  
-      { title: "Fiabilité", para: "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes." },
-  
-      { title: "Respect", para: "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme." },
-  
-      { title: "Service", para: "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme." },
-  
-      { title: "Sécurité", para: "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes." },
+import './Dropdown.css'
 
-    ];
-  
-  
-    return (
-  
-      <div className="my-dropdown">
-  
-        {data.map((user) => (
-  
-          <div className="user">{user}</div>
-  
-        ))}
-  
-      </div>
-  
-    );
-  
+const MyComponent = ({ datas }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const handleArrowClick = () => {
+    setIsExpanded(!isExpanded);
   };
+
+  return (
+    <div className='my-dropdown'>
+      <div className='menu'>
+        <p>{datas.title}</p>
+        <span onClick={handleArrowClick} style={{ cursor: 'pointer' }}>
+          {isExpanded ? '▲' : '▼'}
+        </span>
+      </div>
+      {isExpanded && (
+        <div className='sous-menu'>
+          <p>{datas.para}</p>
+          <Dropdown datas={datas} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default MyComponent;
--- src/components/Panneau/Panneau.jsx
+++ src/components/Panneau/Panneau.jsx
@@ -1,77 +1,26 @@
 import React from 'react';
-import './Panneau.css'
+import Dropdown from '../Dropdown/Dropdown';
 
+const MyComponent = () => {
 
-function Panneau() {
-    const [open, setOpen] = React.useState(false);
+    const datas = [
+  
+      { title: "Fiabilité", para: "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes." },
+  
+      { title: "Respect", para: "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme." },
+  
+      { title: "Service", para: "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme." },
+  
+      { title: "Sécurité", para: "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes." },
 
-    const handleOpen = () => {
-      setOpen(!open);
-    };
-    
+    ];
+
     return (
-        <div className="my-dropdown">
-            <button onClick={handleOpen}>Fiabilité</button>
-
-                {open ? (
-                    <ul className="menu">
-                        <li className="menu-item">
-                            <p>
-                            Les annonces postées sur Kasa garantissent une fiabilité totale. 
-                            Les photos sont conformes aux logements, et toutes les informations 
-                            sont régulièrement vérifiées par nos équipes.
-                            </p>
-                        </li>
-                    </ul>
-                ) : null}
-                {open ? <div className="menu-open"> </div> : <div className="menu-close"> </div>}
-
-            <button onClick={handleOpen}>Respect</button>
-                {open ? (
-                    <ul className="menu">
-                        <li className="menu-item">
-                            <p>
-                            La bienveillance fait partie des valeurs fondatrices de Kasa.
-                            Tout comportement discriminatoire ou de perturbation du voisinage
-                            entraînera une exclusion de notre plateforme.
-                            </p>
-                        </li>
-                    </ul>
-                ) : null}
-                {open ? <div className="menu-open"> </div> : <div className="menu-close"> </div>}
-
-            <button onClick={handleOpen}>Service</button>
-                {open ? (
-                    <ul className="menu">
-                        <li className="menu-item">
-                            <p>
-                            La bienveillance fait partie des valeurs fondatrices de Kasa.
-                            Tout comportement discriminatoire ou de perturbation du voisinage
-                            entraînera une exclusion de notre plateforme.
-                            </p>
-                        </li>
-                    </ul>
-                ) : null}
-                {open ? <div className="menu-open"> </div> : <div className="menu-close"> </div>}
-
-            <button onClick={handleOpen}>Sécurité</button>
-                {open ? (
-                    <ul className="menu">
-                        <li className="menu-item">
-                            <p>
-                            La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs,
-                            chaque logement correspond aux critères de sécurité établis par nos services. En laissant une
-                            note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les 
-                            standards sont bien respectés. Nous organisons également des ateliers sur la 
-                            sécurité domestique pour nos hôtes.
-                            </p>
-                        </li>
-                    </ul>
-                ) : null}
-                {open ? <div className="menu-open"> </div> : <div className="menu-close"> </div>}
-
+        <div>
+          <Dropdown datas={datas} />
         </div>
     );
+
 }
 
-export default Panneau
+export default MyComponent;
