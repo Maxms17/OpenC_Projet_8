8d1359f889cb333aa375d699f24158f411c25d2f
Maxms17 <61410393+Maxms17@users.noreply.github.com>
2023-07-05 14:51:59 +0200

    Page Erreur

--- src/pages/Erreur/Erreur.jsx
+++ src/pages/Erreur/Erreur.jsx
@@ -1,10 +1,23 @@
 import React from "react"
+import { Link } from 'react-router-dom';
 import Layout from "../../components/Layout"
+
+import './Erreur.css'
 
 function Erreur() {
     return (
         <Layout>
-            <p>Page d'Erreur</p>
+            <div className="Erreur-404">
+                <p>404</p>
+            </div>
+            <div className="Erreur-text">
+                <p>Oups! La page que vous demandez n'existe pas.</p>
+            </div>
+            <div className="Erreur-lien">
+                <Link to={`/`}>
+                    <p>Retourner sur la page d'accueil</p>
+                </Link>
+            </div>
         </Layout>
     )
 }
